e1bca1916cd803433f9b44528943108ad0074d43
TXzzera <brunoteixeira.10@hotmail.com>
2025-12-05 00:35:05 -0300

    TTT

--- controllers/shirts.js
+++ controllers/shirts.js
@@ -2,7 +2,7 @@ const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-  //#swagger.tags = ['Shirts']
+  //#swagger.tags = ['shirts']
   try {
     const shirts = await mongodb
       .getDatabase()
@@ -21,26 +21,26 @@ const getAll = async (req, res) => {
 
 
 const getSingle = async (req, res) => {
-  //#swagger.tags = ['Shirts']
+  //#swagger.tags = ['shirts']
   try {
     if (!ObjectId.isValid(req.params.id)) {
-      return res.status(400).json({ message: 'Must use a valid Shirt id to find an Shirt.' });
+      return res.status(400).json({ message: 'Must use a valid shirt id to find an shirt.' });
     }
 
     const shirtId = new ObjectId(req.params.id);
 
-    const Shirt = await mongodb
+    const shirt = await mongodb
       .getDatabase()
       .db()
       .collection('shirts')
-      .findOne({ _id: ShirtId });
+      .findOne({ _id: shirtId });
 
-    if (!Shirt) {
-      return res.status(404).json({ message: "Shirt not found" });
+    if (!shirt) {
+      return res.status(404).json({ message: "shirt not found" });
     }
 
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(Shirt);
+    res.status(200).json(shirt);
 
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -48,72 +48,70 @@ const getSingle = async (req, res) => {
 };
 
 const shirtCreate = async (req,res) =>{
-    //#swagger.tags = ['Shirts']
-    const newShirt = {
+    //#swagger.tags = ['shirts']
+    const newshirt = {
         brand: req.body.brand,
         club: req.body.club,
         year: Number(req.body.year),
         model: req.body.model,
-        size: req.body.size,
         color: req.body.color,
         image: req.body.image
     };
 
     try {
-        const result = await mongodb.insertOne('shirts', newShirt);
+        const result = await mongodb.insertOne('shirts', newshirt);
         res.status(201).json({
-            message: 'Shirt created successfully',
+            message: 'shirt created successfully',
             insertedId: result.insertedId,
-            data: newShirt
+            data: newshirt
         });
     } catch (err) {
-        res.status(500).json({message: 'Creating a Shirt failed! Internal Error.'
+        res.status(500).json({message: 'Creating a shirt failed! Internal Error.'
         });
     }
 }
 
 const shirtUpdate = async (req, res) => {
-    //#swagger.tags = ['Shirts']
-    const ShirtId = new ObjectId(req.params.id);
-    const updatedShirt = {
+    //#swagger.tags = ['shirts']
+    const shirtId = new ObjectId(req.params.id);
+    const updatedshirt = {
         brand: req.body.brand,
         club: req.body.club,
         year: Number(req.body.year),
         model: req.body.model,
-        size: req.body.size,
         color: req.body.color,
         image: req.body.image
     };
 
     try {
-        const query = {_id: ShirtId};
-        const updateData = {$set: updatedShirt};
+        const query = {_id: shirtId};
+        const updateData = {$set: updatedshirt};
         const result = await mongodb.updateOne('shirts', query, updateData);
 
         if (result.modifiedCount > 0) {
         res.status(200).json({
-            message: 'Shirt updated successfully',
-            data: updatedShirt
+            message: 'shirt updated successfully',
+            data: updatedshirt
         });
     } else {
-        res.status(400).json({message: 'Shirt not found!'});
+        res.status(400).json({message: 'shirt not found!'});
     } } catch (err) {
-        res.status(500).json({message: 'Updating Shirt failed! Internal Error.'});
+        res.status(500).json({message: 'Updating shirt failed! Internal Error.'});
 }};
 
 const shirtDelete = async (req, res) => {
-    //#swagger.tags = ['Shirts']
-    const ShirtId = new ObjectId(req.params.id);
+    //#swagger.tags = ['shirts']
+    const shirtId = new ObjectId(req.params.id);
 
     try {
-        const query = {_id: ShirtId};
+        const query = {_id: shirtId};
         const result = await mongodb.deleteOne('shirts', query);
         if (result.deletedCount > 0) {
-            res.status(200).json({message: 'Shirt deleted successfully'});
+            res.status(200).json({message: 'shirt deleted successfully'});
         } else {
-            res.status(400).json({message: 'Shirt not found!'});
+            res.status(400).json({message: 'shirt not found!'});
         } } catch (err) {
-        res.status(500).json({message: 'Deleting Shirt failed!'});
+        res.status(500).json({message: 'Deleting shirt failed!'});
 }};
 
 
